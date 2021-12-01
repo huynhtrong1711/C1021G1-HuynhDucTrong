550cdd23c2844fd9ecbeb3dfbf12993b0f31f4dc
huynhtrong1711 <trongoliver1998@gmail.com>
2021-12-01 20:58:56 +0700

    ngay 01/12/2021

---
+++ module2/src/introduction_to_Java/bai_tap/Money.java
@@ -0,0 +1,16 @@
+package introduction_to_Java.bai_tap;
+
+import java.util.Scanner;
+
+public class Money {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usa;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số tiền USD: ");
+        usa = scanner.nextDouble();
+        double change = usa * vnd;
+        System.out.println("Số tiền VND là: " + change);
+
+    }
+}
---
+++ module2/src/introduction_to_Java/bai_tap/Number.java
@@ -0,0 +1,48 @@
+package introduction_to_Java.bai_tap;
+
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập vào một số: ");
+        int number = scanner.nextInt();
+        String alphabet;
+        switch (number) {
+            case 0:
+                alphabet = "Zero";
+                break;
+            case 1:
+                alphabet = "One";
+                break;
+            case 2:
+                alphabet = "Two";
+                break;
+            case 3:
+                alphabet = "Three";
+                break;
+            case 4:
+                alphabet = "Four";
+                break;
+            case 5:
+                alphabet = "Five";
+                break;
+            case 6:
+                alphabet = "Six";
+                break;
+            case 7:
+                alphabet = "Seven";
+                break;
+            case 8:
+                alphabet = "Eight";
+                break;
+            case 9:
+                alphabet = "Nine";
+                break;
+            default:
+                alphabet = "Out of ability";
+                break;
+        }
+        System.out.println(alphabet);
+    }
+}
